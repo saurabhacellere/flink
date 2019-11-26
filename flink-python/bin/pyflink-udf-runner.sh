f18a624a2ff9631477fcd2e5d75950b5df2343ce
saurabh.patil@acellere.com <saurabh.patil@acellere.com>
2019-11-26 16:31:04 +0530

    commitMessage : [FLINK-14019][python] add support for managing environment and dependencies of Python UDF in Flink Python API

--- flink-python/bin/pyflink-udf-runner.sh
+++ flink-python/bin/pyflink-udf-runner.sh
@@ -23,9 +23,19 @@ if [[ "$FLINK_TESTING" = "1" ]]; then
     FLINK_SOURCE_ROOT_DIR=`cd $ACTUAL_FLINK_HOME/../../../../../; pwd`
     FLINK_PYTHON="${FLINK_SOURCE_ROOT_DIR}/flink-python"
     if [[ ! -f "${FLINK_PYTHON}/pyflink/fn_execution/boot.py" ]]; then
-      # use pyflink source code to override the pyflink.zip in PYTHONPATH
-      # to ensure loading latest code
-      export PYTHONPATH="$FLINK_PYTHON:$PYTHONPATH"
+        # use pyflink source code to override the pyflink.zip in PYTHONPATH
+        # to ensure loading latest code
+        export PYTHONPATH="$FLINK_PYTHON:$PYTHONPATH"
+    fi
+fi
+
+if [[ "$_PYTHON_WORKING_DIR" != "" ]]; then
+    # set current working directory to $_PYTHON_WORKING_DIR
+    cd "$_PYTHON_WORKING_DIR"
+    if [[ "$python" == ${_PYTHON_WORKING_DIR}* ]]; then
+        # The file extracted from archives may not preserve its original permission.
+        # Set minimum execution permission to prevent from permission denied error.
+        chmod +x "$python"
     fi
 fi
 
